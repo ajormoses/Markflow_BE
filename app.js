d83d463638749662294a91d06d9cd463eb5c3ea4
ajormoses <ajormoses@gmail.com>
2026-06-23 13:33:19 +0100

    feat: add dependencies for authentication and error handling, configure middleware

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 import express from "express";
+import cookieSession from "cookie-session";
+import cors from 'cors'
+import { errorHandler } from "./src/middleware/errorhandler.js";
 
 
 const app = express();
 
+app.use(cors(
+    {
+        origin: '*',
+        optionsSuccessStatus: 200
+    }
+))
+app.set('trust proxy', 1) // trust first proxy
+
 app.use(express.json());
+app.use(cookieSession({
+    signed: false,
+    secure: false,
+}))
+
+
+
+app.use(errorHandler);
 
 
 
