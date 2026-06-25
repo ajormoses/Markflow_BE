d1d6e65f10d9ccc858a1964c397f5a82ce4845d5
ajormoses <ajormoses@gmail.com>
2026-06-25 12:06:54 +0100

    feat: add language constants for multilingual support

---
+++ src/constants/enums/lang.js
@@ -0,0 +1,99 @@
+const languages = [
+    "en", // English
+
+    "fr", // French
+
+    "es", // Spanish
+
+    "de", // German
+
+    "it", // Italian
+
+    "pt", // Portuguese
+
+    "nl", // Dutch
+
+    "sv", // Swedish
+
+    "no", // Norwegian
+
+    "da", // Danish
+
+    "fi", // Finnish
+
+    "pl", // Polish
+
+    "cs", // Czech
+
+    "sk", // Slovak
+
+    "hu", // Hungarian
+
+    "ro", // Romanian
+
+    "bg", // Bulgarian
+
+    "hr", // Croatian
+
+    "sr", // Serbian
+
+    "sl", // Slovenian
+
+    "el", // Greek
+
+    "tr", // Turkish
+
+    "uk", // Ukrainian
+
+    "ru", // Russian
+
+    "ar", // Arabic
+
+    "he", // Hebrew
+
+    "fa", // Persian
+
+    "ur", // Urdu
+
+    "hi", // Hindi
+
+    "bn", // Bengali
+
+    "ta", // Tamil
+
+    "te", // Telugu
+
+    "ml", // Malayalam
+
+    "mr", // Marathi
+
+    "gu", // Gujarati
+
+    "pa", // Punjabi
+
+    "zh", // Chinese
+
+    "ja", // Japanese
+
+    "ko", // Korean
+
+    "th", // Thai
+
+    "vi", // Vietnamese
+
+    "id", // Indonesian
+
+    "ms", // Malay
+
+    "tl", // Filipino
+
+    "sw", // Swahili
+
+    "yo", // Yoruba
+
+    "ig", // Igbo
+
+    "ha"  // Hausa
+]
+
+export default languages
